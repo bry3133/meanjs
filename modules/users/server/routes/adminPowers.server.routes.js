8287c3c85c576a6be4931999804eec4ff0f4949f
Nicholas Miller <nickbryanmiller@gmail.com>
2017-11-27 20:30:22 -0500

    more route safety

--- modules/users/server/routes/adminPowers.server.routes.js
+++ modules/users/server/routes/adminPowers.server.routes.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var adminPowersPolicy = require('../policies/adminPowers.server.policy');
+
 module.exports = function (app) {
   var adminPowers = require('../controllers/adminPowers.server.controller');
 
-  app.route('/api/admin/updateUser/:userId').put(adminPowers.updateUser);
-  app.route('/api/admin/deleteUser/:userId').delete(adminPowers.deleteUser);
+  app.route('/api/admin/updateUser/:userId').put(adminPowersPolicy.isAllowed, adminPowers.updateUser);
+  app.route('/api/admin/deleteUser/:userId').delete(adminPowersPolicy.isAllowed, adminPowers.deleteUser);
 
   app.param('userId', adminPowers.participantByID);
 };
